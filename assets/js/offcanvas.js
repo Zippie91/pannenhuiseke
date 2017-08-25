0c6df94e8a4bbc4f4ba3af991e83c38f619bc884
Raf Smits <rafsmits@gmail.com>
2017-08-25 12:27:15 +0000

    Finish Offcanvas - Sidbar Layout

--- assets/js/offcanvas.js
+++ assets/js/offcanvas.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
   $('[data-toggle="offcanvas"]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
+    $('.row-offcanvas').toggleClass('active');
+    $('.toggleIcon').toggleClass('fa-angle-double-right');
+    $('.toggleIcon').toggleClass('fa-angle-double-left');
   });
 });

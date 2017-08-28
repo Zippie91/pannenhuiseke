d9bb1eb55b1ab81d48e31bae46ffc78aa89f97f2
Raf Smits <rafsmits@gmail.com>
2017-08-28 11:25:55 +0000

    create and include php navbar

---
+++ assets/php/navbar.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-light navbar-expand-lg bg-light fixed-top">
+        <a href="/pannenhuiseke" class="navbar-brand">
+            <img src="/pannenhuiseke/assets/images/PanLogo.png" height=30 width=auto alt="Pannenhuiseke VK" class="d-inline-block align-top"></img>
+            Pannenhuiseke
+        </a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#menuCollapsable" aria-controls="menuCollapsable">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="menuCollapsable">
+            <ul class="nav navbar-nav mr-auto">
+                <li class="nav-item"><a href="/pannenhuiseke" class="nav-link active">Home</a></li>
+                <li class="nav-item"><a href="club/" class="nav-link">Club</a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Ploegen</a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="ploegen/h1/">Heren 1</a>
+                      <a class="dropdown-item" href="ploegen/h2/">Heren 2</a>
+                      <a class="dropdown-item" href="ploegen/d1/">Dames </a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="/ploegen/ampersand/">Ampersand</a>
+                    </div>
+                  </li>
+                <li class="nav-item"><a href="leden/" class="nav-link">Leden</a></li>
+                <li class="nav-item"><a href="Sponsors/" class="nav-link">Sponsors</a></li>
+            </ul>
+            <div class="dropdown-divider"></div>
+            <form class="form-inline my-2 my-lg-0 justify-content-end">
+                <input class="form-control mr-sm-2" type="text" placeholder="Zoeken" aria-label="Zoeken" />
+                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Zoeken</button>
+            </form>
+        </div>
+    </nav>
--- index.php
+++ index.php
@@ -19,38 +19,7 @@
     <link rel="stylesheet" href="/pannenhuiseke/assets/css/main.css" type="text/css" />
 </head>
 <body>
-    <nav class="navbar navbar-light navbar-expand-lg bg-light fixed-top">
-        <a href="#" class="navbar-brand">
-            <img src="/pannenhuiseke/assets/images/PanLogo.png" height=30 width=auto alt="Pannenhuiseke VK" class="d-inline-block align-top"></img>
-            Pannenhuiseke
-        </a>
-        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#menuCollapsable" aria-controls="menuCollapsable">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="menuCollapsable">
-            <ul class="nav navbar-nav mr-auto">
-                <li class="nav-item"><a href="#" class="nav-link active">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Club</a></li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Ploegen</a>
-                    <div class="dropdown-menu">
-                      <a class="dropdown-item" href="#">Heren 1</a>
-                      <a class="dropdown-item" href="#">Heren 2</a>
-                      <a class="dropdown-item" href="#">Dames </a>
-                      <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="#">Ampersand</a>
-                    </div>
-                  </li>
-                <li class="nav-item"><a href="#" class="nav-link">Leden</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Sponsors</a></li>
-            </ul>
-            <div class="dropdown-divider"></div>
-            <form class="form-inline my-2 my-lg-0 justify-content-end">
-                <input class="form-control mr-sm-2" type="text" placeholder="Zoeken" aria-label="Zoeken" />
-                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Zoeken</button>
-            </form>
-        </div>
-    </nav>
+    <?php include("assets/php/navbar.php"); ?>
     
     <div class="container">
         <div class="row row-offcanvas row-offcanvas-right">
